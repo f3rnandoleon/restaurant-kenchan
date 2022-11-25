77fa1f258cda81b4c0a0169bf82def9464ed88e9
kevin alex <frezzeg568@gmail.com>
2022-11-24 21:43:11 -0400

    componente_inicio

---
+++ src/componentes/Inicio.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/inicio.css'
+function Inicio() {
+  return (
+    <main class="fondo">
+        <img src="src/assets/logo.png" alt="" class="image"/>
+        <div class="buttons">
+            <button>MENU</button>
+            <button>MAS INFORMACION</button>
+        </div>
+    </main>
+  )
+}
+
+export {Inicio} 
